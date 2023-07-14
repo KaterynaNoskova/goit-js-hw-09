cdcdf8730097ee88b8bc04b77c8bf875d8e8bd1a
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-07-14 20:27:33 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-console.log("hello world!");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+let counter = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+function onStart(){
+    counter = setInterval(chooseBgColor, 1000);
+    startBtn.toggleAttribute('disabled');
+    stopBtn.removeAttribute('disabled');
+};
+
+function onStop(){
+    clearInterval(counter);
+    startBtn.removeAttribute('disabled');
+    stopBtn.toggleAttribute('disabled');
+};
+
+function chooseBgColor (){
+    bodyColor.style.background = getRandomHexColor();
+};
+
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
+
+
